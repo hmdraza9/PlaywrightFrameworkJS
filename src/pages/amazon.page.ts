e2dc649136598e9a64122d8b1645acfbdac66e57
Hamid Raza <hmdraza9@gmail.com>
2024-11-01 20:03:27 +0530

    Added amazon product new page etc.

--- src/pages/amazon.page.ts
+++ src/pages/amazon.page.ts
@@ -1,5 +1,5 @@
 //amazon.page.ts
-import { expect, type Page } from  '@playwright/test';
+import { BrowserContext, expect, type Page } from  '@playwright/test';
 export class AmazonClass{
     readonly page: Page
     constructor(page:Page){
@@ -11,7 +11,7 @@ export class AmazonClass{
     async pressEnter(){
        await this.page.keyboard.press('Enter');
     }
-    async searchResult(expAmazonTitle){
+    async searchResult(expAmazonTitle: String){
       await this.page.waitForLoadState();
       await this.page.locator('//h2[contains(text(),"Results")]').waitFor();
       await this.page.locator('//h2[contains(text(),"Results")]').textContent();
@@ -19,11 +19,54 @@ export class AmazonClass{
       // Capture screenshot with a single timestamped filename
       const timestamp = Date.now();
       await this.page.screenshot({ path: `Screenshots/Amazon_${timestamp}.png`, fullPage: true });
-      console.log(`Screenshot saved as Amazon_${timestamp}.png`);
+      // console.log(`Screenshot saved as Amazon_${timestamp}.png`);
 
       // Check if the page title contains "Amazon"
       const title = await this.page.title();
-      await expect(title).toBe(expAmazonTitle);
+      expect(title).toBe(expAmazonTitle);
       
     }
+    async buyProduct(context: BrowserContext, product: string){
+
+      function getProductCode(url) {
+        // Use a regular expression to match the ASIN pattern after "/dp/"
+        const match = url.match(/\/dp\/([A-Z0-9]{10})/);
+        return match ? match[1] : null;
+      }
+      
+
+      await this.page.waitForLoadState();
+      const productLabelLocator = this.page.locator(`//h2/a/span[text()="${product}"]`);
+      const productName = await productLabelLocator.textContent();
+      // console.log(productName);
+      const productLink = this.page.locator(`//h2/a/span[text()="${product}"]/parent::a`);
+      // console.log(await productLink.getAttribute('href'));
+
+      const [newPage] = await Promise.all([
+
+          context.waitForEvent('page'), productLink.click()
+
+      ]);
+
+      await newPage.waitForLoadState();
+      
+      console.log(await newPage.title());
+      const newPageUrl = newPage.url();
+      console.log(`Page URL: ${newPageUrl}`);
+
+      const productCode = getProductCode(newPageUrl);
+
+      console.log(`Product code: ${productCode}`);
+
+      await newPage.locator(`//div[@data-csa-c-asin='${productCode}']/i[contains(@class,'inactive')]`).click();
+      
+      await newPage.locator("span#productTitle").waitFor();
+
+      const productLable = newPage.locator("span#productTitle");
+
+      console.log(productLable.textContent());
+
+    }
+
+    
 }
