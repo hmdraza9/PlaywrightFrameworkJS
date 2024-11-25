27765f7a5898db244d9dd44cea60a42cdc9ac3af
Hamid Raza <hmdraza9@gmail.com>
2024-11-26 00:44:49 +0530

    Screenshots in masked or element bases, quality set
    Screenshots in masked or element bases, quality set

--- src/pages/google.page.ts
+++ src/pages/google.page.ts
@@ -1,10 +1,16 @@
 //google.page.ts
 import { expect, type Page } from  '@playwright/test';
 export class GoogleClass{
-    readonly page: Page
+    readonly page: Page;
+    private i: number = 0;
     constructor(page:Page){
         this.page=page
     }
+
+    private getNumber(){
+            this.i +=1;
+            return this.i;
+        }
     
     async typeSearchText(searchTerm){
        await this.page.type('textarea[name="q"]',searchTerm)
@@ -14,10 +20,46 @@ export class GoogleClass{
     }
     async searchResult(searchTerm){
       this.page.waitForLoadState();
-      const fileName = `Screenshots/Google_${Date.now()}.png`;
-      await this.page.screenshot({ path: fileName, fullPage: true });
+      let fileName = `Screenshots/Google_${Date.now()+"_"+this.getNumber()}.png`;
+      await this.page.screenshot({ path: fileName, fullPage: true });           //full screen
+      fileName = `Screenshots/Google_${Date.now()+"_"+this.getNumber()}.png`;   // no fullscreen
+      await this.page.screenshot({ path: fileName, fullPage: false });
       console.log(`Screenshot saved as ${fileName}`);
       const title = await this.page.locator("//title").textContent();
+
+     await this.page.locator("textarea[name='q']").screenshot({ path: `Screenshots/Google_${Date.now()+"_"+this.getNumber()}.png` }); //Element level screenshot
+
+     //Mask sensitive info element, applies a strip of pink colour to element, that's it:
+     await this.page.screenshot({
+       path: `Screenshots/Google_Masked_${Date.now()+"_"+this.getNumber()}.png`,
+       mask: [this.page.locator('textarea[name="q"]')]
+     });
+
+        // Controlled quality/size of screenshots
+      await this.page.screenshot({
+          path: `Screenshots/Google_quality_1_${Date.now()+"_"+this.getNumber()}.png`,
+          quality: 1,
+          type: 'jpeg'
+       });
+
+      await this.page.screenshot({
+          path: `Screenshots/Google_quality_33_${Date.now()+"_"+this.getNumber()}.png`,
+          quality: 33,
+          type: 'jpeg'
+       });
+
+      await this.page.screenshot({
+          path: `Screenshots/Google_quality_66_${Date.now()+"_"+this.getNumber()}.png`,
+          quality: 66,
+          type: 'jpeg'
+       });
+
+      await this.page.screenshot({
+          path: `Screenshots/Google_quality_100_${Date.now()+"_"+this.getNumber()}.png`,
+          quality: 100,
+          type: 'jpeg'
+       });
+
       console.log(title);
       const expTitle = `${searchTerm} - Google Search`;
       expect(title).toContain(expTitle);
